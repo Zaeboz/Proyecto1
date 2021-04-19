d3f3e45d690583696c6253038e7d75e455ab611c
Juan Enmanuel <juanenmanel@MacBook-Air.local>
2021-04-19 14:38:11 -0500

    Modificacion de botones

--- src/controladores/ControladorPrincipal.java
+++ src/controladores/ControladorPrincipal.java
@@ -11,7 +11,6 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
 
 
 public class ControladorPrincipal implements Initializable{
@@ -22,18 +21,6 @@ public class ControladorPrincipal implements Initializable{
 	@FXML BorderPane mainPane;
 	BorderPane pane = new BorderPane();;
 	Main main;
-	
-//	procesos.setOnAction(new EventHandler<ActionEvent>() {
-//	    @Override
-//	    public void handle(ActionEvent event) {
-//	        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-//	        // OR, if you define btn as @FXML private Button btn.
-//	        Stage stage = (Stage) btn.getScene().getWindow();
-//	        // these two of them return the same stage
-//	        stage.setWidth(new_val);
-//	        stage.setHeight(new_val);
-//	    }
-//	});
 	
 	@FXML private void cargarVistaProcesos(ActionEvent event) {
 		mainPane.setCenter(null);
